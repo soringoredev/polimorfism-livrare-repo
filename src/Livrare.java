1d45b1d6a071f4c7673017c216e4384448d36ac4
Sorin Gore <soringoredev@gmail.com>
2025-10-13 17:52:31 +0300

    Add instructions

--- src/Livrare.java
+++ src/Livrare.java
@@ -1,2 +1,30 @@
 public class Livrare {
+    /*
+    1. Clasa Livrare va ava mai multe versiuni ale metodei trimitePachet() pentru diferite situatii.
+    2.
+   2.1 * clientul trimite fara detalii(standart)
+   2.2 * clientul specifica adresa
+   2.3 * clientul adauga greutatea
+   2.4 * clientul adauga prioritatea (gen Urgent sau nu. 1 zi/ 3 zile./ 10 zile)
+
+
+    Adica in primul caz avem parantezele goale in celelallte cazuri avem fie doar adresa, fie adresa + greutate,
+    fie adresa + greutate + prioritate.
+
+CERINTE:
+
+Cristi face metoda standart trimitePachet() si mai creeaza o clasa in care vom avea PSVM-ul
+in care apeleaza metoda creata de eel in clasa Livrare.
+
+Victor Creeaza metoda care adauga + adresa.  + apelare in clasa creata de Cristi
+Tudor creeaza metoda care adauga + greutatea + apelare in clasa creata de Cristi
+
+DACA TUDOR NU VA CREA punctul 2.3 -> Cu 1/2/3 ore inainte de lectia de miercuri Cristi efectueaza git pull si daca lucrul lui Tudor
+lipseste el efectueaza in locul lui punctul 2.3  + apelare
+
+Victor - tu trebuie sa faci si punctul 2.4 + apelare in clasa creata de Cristi
+
+
+
+     */
 }
